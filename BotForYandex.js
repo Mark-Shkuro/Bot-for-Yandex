b622f19ccfb18ad022d5d66ddf9d3218a57132af
Mark-Shkuro <76617542+Mark-Shkuro@users.noreply.github.com>
2021-04-22 13:54:03 +0300

    Update BotForYandex.js

--- BotForYandex.js
+++ BotForYandex.js
@@ -1,14 +1,13 @@
 let keywords = ["Гобой","Флейта","Как звучит флейта","Фагот","Кларнет"];
 let keyword = keywords[Math.floor(Math.random()* keywords.length)];
-let buttonSearch = document.getElementsByClassName('button_js_inited')[0];
+let buttonSearch = document.querySelector(".button_theme_search");
 if (buttonSearch != undefined){ // Главная страница яндекс или нет
     document.getElementById('text').value = keyword;
     buttonSearch.click();
-} else{
+} else {
     let links = document.links;
     let goToTheNextPage = true;
-    let nextPage = document.getElementsByClassName('pager__item_kind_next')[0];
-    let currentPage = document.getElementsByClassName("pager__item_kind_page")[0].innerText;
+    let currentPage = document.querySelectorAll(".pager__item_current_yes")[0].innerText;
 for(let i = 0; i<links.length; i++){
     let link = links[i];
     if(link.href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai') != -1){
@@ -17,6 +16,8 @@ for(let i = 0; i<links.length; i++){
     break;
   }
  }
-    if(currentPage > 4 || nextPage == undefined) location.href = "https://yandex.ru/"
+    let nextPage = document.querySelector(".pager__item_kind_next");
+    if(currentPage > 4 ) location.href = "https://yandex.ru/"
     else if(goToTheNextPage) nextPage.click();
    }
+
